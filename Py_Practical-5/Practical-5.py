da55a50c47174f6eaa8afd272b2453c3abff398c
Prachi Shah <prachivshah03@gmail.com>
2022-02-04 13:08:29 +0530

    commit

---
+++ Py_Practical-5/Practical-5.py
@@ -0,0 +1,21 @@
+#20CE133 - Prachi Shah
+
+"""You are given a string and your task is to swap cases. In other words, convert all lowercase letters to uppercase letters and vice versa. """
+
+def swap_case(s):
+    
+    Output = '';
+    for char in s:
+        if(char.isupper()==True):
+            Output =Output + (char.lower());
+        elif(char.islower()==True):
+             Output =Output + (char.upper());
+        else:
+             Output =Output + char;
+    return Output;
+  
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
