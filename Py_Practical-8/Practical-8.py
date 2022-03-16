40bc454780fb90c4de7c3a2ae5872d3aad76cde8
Prachi Shah <prachivshah03@gmail.com>
2022-03-16 13:14:35 +0530

    commit

---
+++ Py_Practical-8/Practical-8.py
@@ -0,0 +1,31 @@
+#20CE133 - Prachi Shah
+
+class node:
+    def __init__(self, data, next):
+        self.data = data
+        self.next = next
+
+def push(head, data):
+    new_node = node(data, head)
+    head = new_node
+    return head
+
+def pop(head):
+    if head is None:
+        return None
+    data = head.data
+    head = head.next
+    return data, head
+
+# traversal methods...
+def traverse(head): 
+    while head is not None:
+        print(head.data)
+        head = head.next
+
+head = node(1, None) # Create a node with data 1
+head = push(head, 6) # Push 6 into the stack
+head = push(head, 9) 
+head = push(head, 12) 
+data, head = pop(head) # Pop the stack
+traverse(head) # Print the stack
